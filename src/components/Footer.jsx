ce329206daa928c627c6eea45ce88ea0430e6516
Saadettin Ördekçi <cenbek03@hotmail.com>
2024-10-04 00:22:26 +0300

    app created using vite.js

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="appFooter">
+     </footer>   
+  )
+}
+
+export default React.memo(Footer)
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { APP_HEADER } from './../const/index'
+const Header = () => {
+  return (
+    <header className='appHeader'>
+        <h2>{APP_HEADER}</h2>
+    </header>
+  )
+}
+
+export default Header
